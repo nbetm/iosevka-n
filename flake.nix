f91578c32d4e2823841cd408200af0b07df975da
Nelson R Monserrate <nrmonserrate@gmail.com>
2026-05-17 21:00:45 +0200

    Add nix devshell and .gitignore

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "Iosevka N build environment";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            gh
+            git
+            gnutar
+            gzip
+            nodejs_24
+            ttfautohint
+          ];
+        };
+      }
+    );
+}
